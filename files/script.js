2a9c727d986e3a7acfe9d99a97cb91f3c1311d68
pawel-chudy <39808870+pawel-chudy@users.noreply.github.com>
2018-07-10 18:32:04 +0200

    small corrections

--- files/script.js
+++ files/script.js
@@ -1,7 +1,7 @@
 const salaries = {
     mrWh: {
         intern: 2521,
-        intern: 2602,
+        contractual: 2602,
         designated: 2944,
         certificated: 3433
     },
@@ -20,153 +20,100 @@ const salaries = {
 };
 
 const workload = [];
-
 workload["18"] = 18;
 workload["22"] = 22;
 workload["25"] = 25;
 workload["30"] = 30;
 
 function getSalary() {
-    
     const edLvlVal = document.querySelector('#edLvl').value;
-    
     const degreeVal = document.querySelector('#degrees').value;
     
     return salaries[edLvlVal][degreeVal];
 }
 
-function getDegrees() {
-    
-    let degree = 0;
-    
-    const form = document.forms["data"];
-    
-    const selectedDegree = form.elements["degrees"];
-    
-    degree = degrees[selectedDegree.value];
-    
-    return degree;
-}
-
 function getWorkload() {
-    
     let wkld = 0;
-    
     const form = document.forms["data"];
-    
     const selectedWorkload = form.elements["workload"];
-    
     wkld = workload[selectedWorkload.value];
     
     return wkld;
 }
 
 function chkJobTime(pStr) {
-    
     let splWorkingHours = document.getElementById("splWorkingHours");
     
     if (!isNaN(pStr) && pStr !== "" && pStr <= getWorkload()) {
-        
         splWorkingHours.style.fontSize = "22px";
-        
+        document.getElementById("splWorkingHours").innerHTML = "";
         pStr = pStr / getWorkload();
         
         return pStr;
-        
     } else if (isNaN(pStr)) {
-        
         splWorkingHours.style.fontSize = "18px";
-        
         splWorkingHours.innerHTML = "Enter number." + "<br>";
         
         return 0;
-        
     } else if (pStr > getWorkload()) {
-        
         let splWorkingHours = document.getElementById("splWorkingHours");
-        
         splWorkingHours.innerHTML = "Incorrect value." + "<br>";
         
         return 0;
-        
     } else
-        
         document.getElementById("splWorkingHours").innerHTML = "";
 }
 
 function calcWorkingHours() {
-    
     let str = document.getElementById("inppn");
-    
     let strVal = str.value;
-    
     let cmp = /^\d{1,2}$/.test(strVal);
     
     if (cmp === true) {
-        
         let commaStr = strVal.concat(",0");
-        
         document.getElementById("inppn").value = commaStr;
         
         return chkJobTime(strVal);
-        
     } else {
-        
         strVal = str.value.split(",").join(".");
-        
         let fstrVal = parseFloat(strVal);
-        
         let cmp2 = /^\d{1,2}\.?\d$/.test(strVal);
         
         if (cmp2 === false) {
-            
             strVal = strVal.substring(0, strVal.length - 1);
-
             document.getElementById("inppn").value = strVal;
 
             return chkJobTime(strVal);
         }
-        
         return chkJobTime(fstrVal);
     }
 }
 
 function calcSalaries() {
-
-    if (typeof(getSalary()) === "undefined") 
-        
+    if (typeof(getSalary()) === "undefined")
         document.getElementById("splWage").innerHTML = "Fill in above fields.";
-    
     else
-        
         document.getElementById("splWage").innerHTML = getSalary() + " zł";
     
-    calcWorkingHours();
-    
     let BasicPay = (getSalary() * calcWorkingHours()).toFixed(2);
-    
     document.getElementById("splBasicPay").innerHTML = BasicPay.replace(/\./g, ',') + " zł";
-
-    if (isNaN(BasicPay)) 
-        
+    
+    if (isNaN(BasicPay))
         document.getElementById("splBasicPay").innerHTML = "Fill in above fields.";
     
     document.getElementById("splBasicPay").style.padding = "5px 10px";
 }
 
 function clearInps() {
-
     location.reload();
 }
 
 function stopRKey(evt) {
-    
     var evt = (evt) ? evt : ((event) ? event : null);
     
     var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
     
-    if ((evt.keyCode == 13) && (node.type == "text")) 
-        
+    if ((evt.keyCode == 13) && (node.type == "text"))    
         return false;
 }
 
